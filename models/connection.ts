dfdc5144418b8c8e743a1082487979f419b7afcd
Antony Merle <antony.merle@gmail.com>
2023-05-27 14:50:11 +0200

    connection file

---
+++ models/connection.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const dbConnect = async () => {
+  const connectionString = process.env.DB_CONNECTION_STRING!;
+
+  mongoose
+    .connect(connectionString, { connectTimeoutMS: 3000 })
+    .then(() => console.log("Connecté à la base de données"))
+    .catch((error) => console.log(error));
+};
+
+export default dbConnect;
