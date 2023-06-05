89277cc5073de147dc6944e55e572cb3b59e8fd0
Antony Merle <antony.merle@gmail.com>
2023-06-05 23:23:59 +0200

    renaming

--- utils/getStripe.ts
+++ utils/getStripe.ts
@@ -4,7 +4,7 @@ import { Stripe } from "@stripe/stripe-js";
 let stripePromise: Promise<Stripe | null>;
 
 const getStripe = async () => {
-  const key = `${process.env.NEXT_STRIPE_PUBLISHABLE_KEY}`;
+  const key = `${process.env.STRIPE_PUBLISHABLE_KEY}`;
   if (!stripePromise) stripePromise = loadStripe(key);
 
   return stripePromise;
