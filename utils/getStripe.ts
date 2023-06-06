c65c1575696bbc99303e6776bcf54afa4fbb4070
Antony Merle <antony.merle@gmail.com>
2023-06-06 18:25:36 +0200

    shipping_options for checkout

--- utils/getStripe.ts
+++ utils/getStripe.ts
@@ -10,4 +10,47 @@ const getStripe = async () => {
   return stripePromise;
 };
 
+export const shipping_options = [
+  {
+    shipping_rate_data: {
+      type: "fixed_amount",
+      fixed_amount: {
+        amount: 0,
+        currency: "eur",
+      },
+      display_name: "Free shipping",
+      delivery_estimate: {
+        minimum: {
+          unit: "business_day",
+          value: 5,
+        },
+        maximum: {
+          unit: "business_day",
+          value: 7,
+        },
+      },
+    },
+  },
+  {
+    shipping_rate_data: {
+      type: "fixed_amount",
+      fixed_amount: {
+        amount: 1500,
+        currency: "eur",
+      },
+      display_name: "Next day air",
+      delivery_estimate: {
+        minimum: {
+          unit: "business_day",
+          value: 1,
+        },
+        maximum: {
+          unit: "business_day",
+          value: 1,
+        },
+      },
+    },
+  },
+];
+
 export default getStripe;
