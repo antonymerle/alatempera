753dedf6e5312589b9c1e17f0f24b66c38742d31
Antony Merle <antony.merle@gmail.com>
2023-06-06 23:36:57 +0200

    archivo font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import { Archivo } from "next/font/google";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import style from "../styles/Layout.module.css";
@@ -8,21 +9,25 @@ const { layout } = style;
 
 type LayoutProps = { children?: ReactNode };
 
+const font = Archivo({ subsets: ["latin"] });
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className={layout}>
-      <Head>
-        <title>A la tempera : vente de peinture sur bois pour enfants</title>
-        <meta
-          name="description"
-          content="Exposition et vente de peintures sur bois pour enfants réalisées à la tempera. Idéal pour un cadeaux ou décorer une chambre."
-        />
-      </Head>
-      <Navbar />
-      <main>{children}</main>
-      <footer>
-        <Footer />
-      </footer>
+    <div className={font.className}>
+      <div className={layout}>
+        <Head>
+          <title>A la tempera : vente de peinture sur bois pour enfants</title>
+          <meta
+            name="description"
+            content="Exposition et vente de peintures sur bois pour enfants réalisées à la tempera. Idéal pour un cadeaux ou décorer une chambre."
+          />
+        </Head>
+        <Navbar />
+        <main>{children}</main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
   );
 };
