7c7d726e1bec91efe12c2452bccb385a432dd951
Antony Merle <antony.merle@gmail.com>
2023-06-08 22:04:50 +0200

    Sold out overlay

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import { ICartItem } from "@/context/StateContext";
 import styles from "@/styles/Home.module.css";
 
-const { main, gallery, opus, imgContainer, opusDescription } = styles;
+const { main, gallery, opus, imgContainer, opusDescription, soldOut } = styles;
 
 export const Card: React.FC<ICartItem> = ({
   imgURL,
   priceHT,
   priceTTC,
   title,
+  inventory,
 }) => {
   // const { description, imgURL, priceHT, priceTTC, qty, title } = work;
   return (
     <div className={opus}>
-      <div className={imgContainer}>
+      <div className={`${imgContainer} ${inventory > 0 ? null : soldOut}`}>
         <Image alt={title} fill={true} objectFit="cover" src={imgURL[0]} />
       </div>
       <div className={opusDescription}>
