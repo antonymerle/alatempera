7b1c1d2a1e2fbf8b48efbe59c35ab0d70efc008d
Antony Merle <antony.merle@gmail.com>
2023-06-09 15:48:51 +0200

    set navbar layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,44 +5,56 @@ import Cart from "./Cart";
 import style from "../styles/Navbar.module.css";
 import Link from "next/link";
 
-const { navbarContainer, logo, cartIcon, navIcons, search, cartItemQty } =
-  style;
+const {
+  navbarContainer,
+  logoContainer,
+  logo,
+  cartIcon,
+  search,
+  cartItemQty,
+  menu,
+  logoSide,
+  menuSide,
+} = style;
 
 const Navbar: React.FC = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
-
-  // const { setShowCart } = useStateContext();
-
   return (
-    <nav>
-      <Link href={"/"}>
-        <div className="logo">
-          <Image width={100} height={100} src="/logo.jpg" alt="logo"></Image>
-        </div>
-      </Link>
-      <h1>A la tempera</h1>
-      <ul>
-        <li>A propos</li>
-        <li>Originaux</li>
-        <li>Impressions</li>
-        <li>Carnet</li>
-        <li>
-          <button
-            type="button"
-            className={cartIcon}
-            onClick={() => setShowCart(true)}
-          >
-            <AiOutlineShopping size={24} color="grey" />
+    <nav className={navbarContainer}>
+      <div className={logoSide}>
+        <Link href={"/"}>
+          <div className={logoContainer}>
+            <Image
+              width={100}
+              height={100}
+              src="/logo.jpg"
+              alt="logo"
+              className={logo}
+            ></Image>
+          </div>
+        </Link>
+        <h1>A la tempera</h1>
+      </div>
 
-            <span
-            // className={cartItemQty}
+      <div className={menuSide}>
+        <ul className={menu}>
+          <li>A propos</li>
+          <li>Originaux</li>
+          <li>Impressions</li>
+          <li>Carnet</li>
+          <li>
+            <button
+              type="button"
+              className={cartIcon}
+              onClick={() => setShowCart(true)}
             >
-              {totalQuantities}
-            </span>
-          </button>
-        </li>
-      </ul>
-      {showCart && <Cart />}
+              <AiOutlineShopping size={30} />
+              <span>{totalQuantities}</span>
+            </button>
+          </li>
+        </ul>
+        {showCart && <Cart />}
+      </div>
     </nav>
   );
 };
