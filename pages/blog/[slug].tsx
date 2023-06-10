7c2a116b2dc7c104b93b08fc014fff7896b2ce83
Antony Merle <antony.merle@gmail.com>
2023-06-10 15:26:01 +0200

    slug for blog

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,65 @@
+import fs from "fs";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import Head from "next/head";
+import H1 from "@/components/mdx/H1";
+import H2 from "@/components/mdx/H2";
+import HeroImage from "@/components/mdx/HeroImage";
+import P from "@/components/mdx/P";
+
+export default function PostPage({
+  source,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <div>
+      <Head>
+        <title>{source.frontmatter.title as string}</title>
+      </Head>
+      <MDXRemote
+        {...source}
+        // specifying the custom MDX components
+        components={{
+          h1: H1,
+          h2: H2,
+          p: P,
+          HeroImage,
+        }}
+      />
+    </div>
+  );
+}
+export async function getStaticPaths() {
+  return { paths: [], fallback: "blocking" };
+}
+
+export async function getStaticProps(
+  ctx: GetStaticPropsContext<{
+    slug: string;
+  }>
+) {
+  const { slug } = ctx.params!;
+
+  // retrieve the MDX blog post file associated
+  // with the specified slug parameter
+  const postFile = fs.readFileSync(`_posts/${slug}.mdx`);
+
+  // MDX content gets converted into JSX by the serialize() function
+  // read the MDX serialized content along with the frontmatter
+  // from the .mdx blog post file
+  const mdxSource = await serialize(postFile, { parseFrontmatter: true });
+  return {
+    props: {
+      source: mdxSource,
+    },
+    /*
+      Enable ISR
+      Since blog posts are likely to be updated over time,
+      we use the Incremental Static Regeneration (ISR) approach.
+      That is enabled through the revalidate option and allows static pages
+      to be incrementally updated without requiring a complete rebuild of the Next.js app.
+
+    */
+    revalidate: 60,
+  };
+}
