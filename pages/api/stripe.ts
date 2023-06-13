b668fb43187e73f0b3cf1d225a718c721b1e198b
Antony Merle <antony.merle@gmail.com>
2023-06-13 15:36:02 +0200

    pass custom metadata to discriminate product type

--- pages/api/stripe.ts
+++ pages/api/stripe.ts
@@ -18,14 +18,18 @@ export default async function handler(
     //   process.env.DOMAIN_NAME + "images" + req.body.cartItems[0].imgURL[0]
     // );
 
+    // cf documentation : line_items : https://stripe.com/docs/api/checkout/sessions/object#checkout_session_object-line_items
     try {
       const line_items = req.body.cartItems.map((item: ICartItem) => {
         return {
+          // price_data : https://stripe.com/docs/api/subscriptions/update#update_subscription-items-price_data
+          // description: item.type,
           price_data: {
             currency: "eur",
             unit_amount: Math.floor(item.priceTTC * 100),
             product_data: {
               name: item.title,
+              description: item.type,
               images: [
                 process.env.DOMAIN_NAME +
                   "_next/" +
@@ -33,14 +37,30 @@ export default async function handler(
                   item.imgURL[0] +
                   "&w=1920&q=75",
               ],
+              metadata: {
+                id: item._id,
+                type: item.type,
+                title: item.title,
+                quantity: item.cartQty,
+              },
             },
           },
           adjustable_quantity: { enabled: true, minimum: 1 },
           quantity: item.cartQty,
+          // price: {
+
+          // },
+          //taxes : https://stripe.com/docs/api/checkout/sessions/object#checkout_session_object-line_items-data-taxes
         };
       });
 
       console.log(line_items[0].price_data.product_data.images[0]);
+
+      const commandDetails = line_items.map(
+        (item: any) => item.price_data.product_data.metadata
+      );
+
+      console.log({ commandDetails });
 
       // Create Checkout Sessions from body params.
       const params = {
@@ -50,6 +70,7 @@ export default async function handler(
         shipping_address_collection: { allowed_countries: ["FR"] },
         shipping_options,
         line_items,
+        metadata: { commandDetails: JSON.stringify(commandDetails) },
         success_url: `${req.headers.origin}/success`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
