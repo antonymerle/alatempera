3519583ee757b875b7356b8c997293a7cb30df08
Antony Merle <antony.merle@gmail.com>
2023-06-16 18:35:59 +0200

    regrouping all types here

---
+++ types/types.ts
@@ -0,0 +1,8 @@
+export type PostPreview = {
+  title: string;
+  description: string;
+  previewImage: string;
+  slug: string;
+};
+
+export type ImageOrientation = "portrait" | "landscape" | "square";
