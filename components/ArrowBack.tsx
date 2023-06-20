48b1553f0f922b0c48f2137eecf8bcd8527b78a2
Antony Merle <antony.merle@gmail.com>
2023-06-20 22:01:11 +0200

    navigation translation

--- components/ArrowBack.tsx
+++ components/ArrowBack.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { MdKeyboardBackspace } from "react-icons/md";
+import useTranslation from "next-translate/useTranslation";
 import style from "../styles/ArrowBack.module.css";
 
 const { container } = style;
 
 const ArrowBack = ({ previousLocation }: { previousLocation?: string }) => {
   const router = useRouter();
+  const { t } = useTranslation("navigation");
+
   const handleGoBack = () => {
     router.back();
   };
   return (
     <div className={container} onClick={handleGoBack}>
-      <MdKeyboardBackspace /> <span>Revenir</span>
+      <MdKeyboardBackspace /> <span>{t("goBack")}</span>
     </div>
   );
 };
