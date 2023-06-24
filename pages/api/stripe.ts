c6ff53ecd7002d28d120f9979ed7eb586f633bbd
Antony Merle <antony.merle@gmail.com>
2023-06-24 21:49:07 +0200

    CHECKOUT_SESSION_ID as success URL param

--- pages/api/stripe.ts
+++ pages/api/stripe.ts
@@ -71,7 +71,7 @@ export default async function handler(
         shipping_options,
         line_items,
         metadata: { commandDetails: JSON.stringify(commandDetails) },
-        success_url: `${req.headers.origin}/success`,
+        success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
 
