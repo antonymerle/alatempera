dc83ad9f1849e469cf19eeaf151997947c560caf
Antony Merle <antony.merle@gmail.com>
2023-06-29 23:01:26 +0200

    added two types for mailer route

--- types/types.ts
+++ types/types.ts
@@ -16,3 +16,16 @@ export type ISOCodes = {
   europeNoFR: string[];
   northAmerica: string[];
 };
+
+export type ContactMessage = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+export type ResMailer = {
+  success: boolean;
+  data: ContactMessage | null;
+  error: string | null;
+};
