246622d718229477be34536a7ee339274ae60d68
Antony Merle <antony.merle@gmail.com>
2023-06-30 14:49:26 +0200

    wiring Privacy component

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import Privacy from "@/components/Privacy";
 
 const privacy = () => {
-  return <div>privacy</div>;
+  return <Privacy />;
 };
 
 export default privacy;
