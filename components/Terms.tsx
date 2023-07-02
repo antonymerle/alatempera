43caae737fea3b4ed70e0bced238fd6e366a0e0e
Antony Merle <antony.merle@gmail.com>
2023-07-02 22:58:30 +0200

    Terms component

---
+++ components/Terms.tsx
@@ -0,0 +1,357 @@
+import React from "react";
+import Link from "next/link";
+import style from "../styles/Terms.module.css";
+
+const { container } = style;
+
+const Terms = () => {
+  return (
+    <div className={container}>
+      <h2>Conditions générales de vente</h2>
+      <p>Les présentes conditions de vente sont conclues :</p>
+      <p>
+        - d’une part par l’Entreprise Individuelle Manon Merle, numéro SIRET
+        95392094900013, dont l’adresse est située à Anglet, France, ci-après
+        dénommée "A LA TEMPERA" et gérant le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link>,
+      </p>
+      <p>
+        - et d’autre part, par toute personne physique ou morale souhaitant
+        procéder à un achat via le site internet www.alatempera.com et dénommée
+        ci-après " l’acheteur ".
+      </p>
+      <h3>Article 1 : Généralités </h3>
+      <p>
+        Les présentes Conditions Générales de Vente (CGV) visent à définir les
+        relations contractuelles entre “A la tempera” et l’acheteur et les
+        conditions applicables à tout achat effectué par le biais du site{" "}
+        internet www.alatempera.com
+      </p>
+      <p>
+        L’acquisition d’une oeuvre à travers le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> implique une acceptation sans
+        réserve par l’acheteur des présentes conditions de vente dont l’acheteur
+        reconnaît avoir pris connaissance préalablement à sa commande.
+      </p>
+      <p>
+        Avant toute transaction, l’acheteur déclare d’une part que l’achat
+        d’oeuvres sur le site <Link href={"/"}>www.alatempera.com</Link> est
+        sans rapport direct avec son activité professionnelle et est limité à
+        une utilisation strictement personnelle, et d’autre part avoir la pleine
+        capacité juridique, lui permettant de s’engager au titre des présentes
+        conditions générales de ventes.
+      </p>
+      <p>
+        “A la tempera” se réserve le droit d’annuler ou de refuser, sans délai,
+        toute commande qui lui paraîtrait litigieuse ou frauduleuse et/ou toute
+        commande par un acheteur avec lequel il existerait un litige passé ou en
+        cours de toute nature.
+      </p>
+      <p>
+        Les présentes conditions sont modifiables à tout moment par “A la
+        tempera” sans préavis. De ce fait, les conditions applicables seront
+        celles en vigueur à la date de la commande par l’acheteur.
+      </p>
+      <h3>Article 2 : Oeuvres </h3>
+      <p>
+        Sera considérée comme « oeuvre », toute peinture originale ou impression
+        présentée sur le site <Link href={"/"}>www.alatempera.com</Link> avec un
+        descriptif mettant l’acheteur en mesure de connaître ses
+        caractéristiques essentielles (titres, dimensions, support, etc…) ainsi
+        que son prix.
+      </p>
+      <p>
+        Les oeuvres sont proposées à l’achat dans la limite des stocks
+        disponibles. Les photographies des oeuvres présentées sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> sont les plus fidèles
+        possibles aux tableaux réels, mais les couleurs qui apparaissent à
+        l’écran peuvent légèrement différer de celles de l’oeuvre réelle. Les
+        photographies des oeuvres présentées sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> n’engagent en rien “A la
+        tempera”.
+      </p>
+      <h3>Article 3 : Prix : </h3>
+      <p>Les œuvres sont vendues non encadrées.</p>
+      <p>
+        Les prix indiqués sont les prix nets à payer en euros, hors frais de
+        port. La TVA est non applicable selon l’article 293 B du CGI.
+      </p>
+      <p>
+        Les frais de port sont facturés en supplément du prix des produits
+        achetés suivant l’adresse de l’acheteur et son pays d’expédition. Les
+        tarifs applicables sont récapitulés en annexe et peuvent faire l’objet
+        de changement à tout moment par “A la tempera”.
+      </p>
+      <p>
+        Dans le cadre d’un achat à destination d’un pays hors Union Européenne,
+        toutes charges, taxes locales éventuelles, ou droits de douanes, ne
+        relèvent pas de “A la tempera” et seront à la charge de l’acheteur. Il
+        est de la responsabilité de l’acheteur de s’informer sur les conditions
+        d’importation et de déclaration auprès des autorités de son pays.
+      </p>
+      <p>
+        “A la tempera” se réserve le droit de modifier ses prix à tout moment,
+        étant toutefois entendu que le prix figurant au catalogue le jour de la
+        commande sera le seul applicable à l’acheteur.
+      </p>
+      <h3>Article 4 : Commande d’une Œuvre </h3>
+      <p>
+        L’achat d’une oeuvre sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> passe par une procédure en
+        plusieurs étapes :
+      </p>
+      <p>
+        Sélection – l’acheteur peut ajouter à son panier une (ou plusieurs)
+        oeuvre qu’il désire acquérir en cliquant sur l’icône «  Ajouter au
+        panier  » accessible sur la page de présentation de l’oeuvre. Le panier
+        reste accessible et modifiable à tout moment lors de la connexion de
+        l’acheteur sur le site d’”A la tempera”, en cliquant sur l’icône
+        « Panier » en haut à droite du site . Toute oeuvre déjà vendue et
+        indiquée comme telle sur la page de présentation de l’oeuvre ne pourra
+        pas être ajoutée au panier.
+      </p>
+      <p>
+        Page « Panier » – Dans le panier, l’acheteur peut sélectionner les
+        oeuvres qu’il désire effectivement acheter en supprimant les oeuvres
+        non-désirées et modifier les quantités dans la limite des stocks
+        disponibles.
+      </p>
+      <p>
+        “Payer”- Après avoir cliqué sur le bouton “payer”, l’acheteur retrouvera
+        le récapitulatif de sa commande et des quantités souhaitées. Les frais
+        de livraison évalués en fonction du pays de livraison renseigné par
+        l’acheteur, les délais de livraison et le montant total de la commande
+        seront indiqués à l’acheteur.
+      </p>
+      <p>
+        L’acheteur renseignera les informations nécessaires au traitement de sa
+        commande (email de contact, nom, pays de livraison, adresse de
+        livraison). Il est possible d’indiquer une adresse de facturation
+        différente de l’adresse de livraison. Tous les champs sont des
+        renseignements obligatoires car nécessaires à la bonne prise en compte
+        de la commande et à la livraison. La politique de traitement des données
+        personnelles est détaillée dans la rubrique “Confidentialité” du site{" "}
+        <Link href={"/"}>www.alatempera.com</Link>. L’acheteur a la possibilité
+        de modifier et supprimer ses informations personnelles à tout moment.
+      </p>
+      <h3>Article 5 : Paiement d’une Œuvre </h3>
+      <p>
+        “A la tempera” propose deux options de paiements au choix à l’acheteur
+        pour régler ses achats sur le site www.alatempera.com : paiement
+        sécurisé par Googlepay ou paiement sécurisé par carte bancaire.
+      </p>
+      <p>
+        Tout paiement doit s’entendre pour un paiement de l’intégralité du prix
+        de l’oeuvre et des frais de livraison.
+      </p>
+      <p>
+        S’il a choisi le paiement par GooglePay, l’acheteur est redirigé sur
+        l’interface sécurisée GoogleePay. S’il a choisi le paiement par carte
+        bancaire (Visa, MasterCard, American Express, autres cartes bleues),
+        l’acheteur renseignera ses références de carte bleue sur le site{" "}
+        sécurisé Stripe garantissant la confidentialité et la sécurité des
+        données et le débit de la carte sera effectué dans les 48h ouvrés
+        suivants la réservation.
+      </p>
+      <p>
+        Si le paiement est accepté, la commande est enregistrée et le contrat
+        définitivement formé. Le paiement par GooglePay ou par carte bancaire
+        est irrévocable.
+      </p>
+      <p>
+        La confirmation d’une commande entraîne acceptation des présentes
+        conditions de vente, la reconnaissance d’en avoir parfaite connaissance
+        et la renonciation à se prévaloir de ses propres conditions d’achat.
+        L’ensemble des données fournies et la confirmation enregistrée vaudront
+        preuve de la transaction. La réception de la commande fera l’objet d’une
+        confirmation de réception automatique par l’envoi d’un courrier
+        électronique à l’acheteur. Le paiement du total dû, à savoir le prix net
+        de l’oeuvre et les frais de livraison, lancera le processus de
+        livraison.
+      </p>
+      <h3>Article 6 : Réserve de propriété </h3>
+      <p>
+        Les oeuvres en vente sur le site www.alatempera.com, restent la
+        propriété de ”A la tempera”, jusqu’à paiement intégral de l’oeuvre et
+        des frais de livraison.
+      </p>
+      <h3>Article 7. Droit de rétractation de 14 jours </h3>
+      <p>
+        En application de l’article L.121-21 (loi du 17 mars 2014 relative à la
+        consommation) du code de la consommation, l’acheteur dispose d’un délai
+        de 14 jours à compter de la livraison de l’oeuvre pour formuler son
+        droit de rétractation auprès de ”A la tempera” via le formulaire de
+        contact du site internet www.alatempera.com L’oeuvre devra être
+        retournée dans son emballage d’origine et en parfait état dans les 7
+        jours de la livraison. L’oeuvre voyagera donc aux risques et périls de
+        l’acheteur.
+      </p>
+      <p>Les frais de retour seront à la charge exclusive de l’acheteur.</p>
+      <p>
+        Les oeuvres retournées endommagées ou salies par l’acheteur (traces de
+        doigt ou traces de toute autre nature) ne seront pas reprises ni
+        remboursées.
+      </p>
+      <p>
+        Le remboursement sera effectué dans un délai de 30 jours à compter de la
+        réception par “A la tempera”, de l’oeuvre retournée en parfait état.
+      </p>
+      <h3>Article 8 : Livraison </h3>
+      <p>
+        Les livraisons sont faites à l’adresse indiquée lors de la commande. Les
+        commandes sont effectuées par La Poste via COLISSIMO, service de
+        livraison avec suivi, remise sans signature.
+      </p>
+      <p>
+        Il n’y a pas de livraison possible pour les destinations suivantes :
+        Samoa américaines, Île Christmas, Îles Cocos (Keeling), Cuba, Îles
+        Heard-et-MacDonald, Iran, Corée du Nord, Îles Marshall, Micronésie, Île
+        Norfolk, Îles Mariannes du Nord, Palaos, Soudan, Syrie, Îles mineures
+        éloignées des États-Unis, Îles Vierges des États-Unis
+      </p>
+      <h4>8.1 Conditionnement de la commande </h4>
+      <p>“A la tempera” n’effectue pas d’expédition d’œuvre encadrée.</p>
+      <p>
+        Toutes les commandes seront livrées avec la facture et conditionnées
+        dans un emballage de protection.
+      </p>
+      <h4>8.2 Délais de livraison </h4>
+      <p>
+        Les livraisons sont effectuées dans un délai moyen de 7 jours en France,
+        et n’excédant que très rarement 30 jours à l’étranger (selon l’adresse
+        de départ et la destination de l’oeuvre). Les délais de livraison ne
+        sont donnés qu’à titre indicatif, leur respect dépend de la Poste. Par
+        conséquent, aucune demande d’indemnisation, de quelque nature que ce
+        soit, ne pourra être réclamée par l’acheteur à “A la tempera” en cas de
+        livraison retardée. Néanmoins, si les délais de livraison dépassaient
+        trente jours à compter de la commande, le contrat de vente pourra être
+        résilié et l’acheteur remboursé.
+      </p>
+      <h4>8.3 Réception de la livraison – droit de retour </h4>
+      <p>
+        L’acheteur est livré à son domicile par son facteur. En cas d’absence de
+        l’acheteur, un avis de passage sera délivré par le facteur, ce qui
+        permet à l’acheteur de retirer la commande au bureau de Poste le plus
+        proche, pendant un délai indiqué par les services postaux. Les risques
+        liés au transport sont à la charge de l'acquéreur à compter du moment où
+        les articles quittent les locaux de “A la tempera”.
+      </p>
+      <p>
+        L’acheteur est tenu de vérifier en présence du préposé de La Poste ou du
+        livreur, l’état de l’emballage de la marchandise et son contenu à la
+        livraison. En cas de dommage pendant le transport, toute protestation
+        doit être effectuée auprès du transporteur.
+      </p>
+      <p>
+        Si l’acheteur constate la non-conformité ou un vice apparent sur
+        l’oeuvre livrée, il doit signaler immédiatement le problème à “A la
+        tempera” via le formulaire de contact du site internet{" "}
+        <Link href={"/"}>www.alatempera.com</Link>
+      </p>
+      <p>
+        Dans cette hypothèse, les frais de retour seront à la charge de “A la
+        tempera” qui procédera aussi au remboursement de l’acheteur dans les
+        plus brefs délais. Une différence de couleurs entre la photographie de
+        l’oeuvre présentée sur le site internet{" "}
+        <Link href={"/"}>www.alatempera.com</Link> et l’oeuvre reçue lors de la
+        livraison ne constitue pas un vice apparent sur l’oeuvre livrée.
+      </p>
+      <p>
+        A défaut d’avoir respecté ces formalités au moment de la livraison, les
+        oeuvres seront réputées conformes et exemptes de tout vice apparent et
+        aucune réclamation ne pourra être valablement acceptée par l’acheteur.
+      </p>
+      <h3>Article 9 : Garanties </h3>
+      <p>
+        L’oeuvre fournie par “A la tempera” bénéficie, conformément aux
+        dispositions légales, de la garantie légale de conformité; la garantie
+        légale contre les vices cachés provenant d’un défaut de matière, de
+        conception, de fabrication, affectant les produits livrés et les rendant
+        impropres à l’utilisation.
+      </p>
+      <p>
+        Toute garantie est exclue en cas de mauvaise utilisation, négligence ou
+        défaut d’entretien de la part de l’acheteur, comme en cas d’usure
+        normale du bien, d’accident ou de force majeure et la garantie de “A la
+        tempera” est limitée au retour et au remboursement de l’oeuvre
+        non-conforme ou affectée d’un vice.
+      </p>
+      <p>
+        “A la tempera” s’engage à ce que les œuvres présentées sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> soient conformes à leurs
+        descriptions.  Dans l’hypothèse d’une erreur matérielle du site{" "}
+        aboutissant à la livraison de la mauvaise oeuvre à l’acheteur,
+        l’acheteur pourra à sa discrétion demander l’échange avec la bonne
+        œuvre, si disponible, une œuvre de valeur équivalente ou le
+        remboursement des sommes versées mais ne pourra prétendre à aucune
+        indemnité de la part de ”A la tempera”.
+      </p>
+      <h3>Article 10 : Propriété intellectuelle </h3>
+      <h4>10.1 Droit de propriété intellectuelle </h4>
+      <p>
+        L’ensemble des éléments visuels exposés sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> sont protégés par le droit
+        d’auteur, en application des articles L.112-1 et suivants du Code de la
+        propriété intellectuelle. L’acheteur ne peut en aucun cas utiliser,
+        copier, reproduire, modifier ou dénaturer cette œuvre ou ses éléments, y
+        compris sur Internet, sans l’autorisation écrite et préalable de “A la
+        tempera”.
+      </p>
+      <h4>10.2 Droit sur l’Œuvre </h4>
+      <p>
+        L’artiste est titulaire des droits d’auteur (droits patrimoniaux et
+        droits moraux) sur sa création, au sens de l’article L.111-1 et suivants
+        du Code de la propriété intellectuelle. Les droits de représentation et
+        de reproduction des œuvres présentes sur le site de “A la tempera”
+        appartiennent à leur auteur. L’autorisation de l’auteur est alors
+        nécessaire pour toute exploitation de ces droits.
+      </p>
+      <p>
+        Les droits de l’acheteur sur l’oeuvre achetée sont donc limités à un
+        droit d’usage privé, excluant tout droit de représentation et de
+        reproduction.
+      </p>
+      <h3>Article 11 : Responsabilité</h3>
+      <p>
+        “A la tempera”, dans le processus de vente à distance, n’est tenue que
+        par une obligation de moyens. Sa responsabilité ne pourra être engagée
+        pour un dommage résultant de l’utilisation du réseau Internet tel que
+        perte de données, intrusion, virus, rupture du service, ou autres
+        problèmes involontaires.
+      </p>
+      <h3>Article 12 : Force majeure </h3>
+      <p>
+        En application de l’article 1148 du Code civil, “A la tempera” ne pourra
+        être tenue comme responsable dans l’exécution de la commande, si tout
+        retard ou tout manquement résulte d’un cas de force majeure.
+      </p>
+      <h3>Article 13 : Données à caractère personnel</h3>
+      <p>
+        "A la tempera" s'engage à préserver la confidentialité des informations
+        fournies par l’acheteur, qu'il serait amené à transmettre pour le
+        traitement de sa commande. Toute information le concernant est soumise
+        aux dispositions de la loi n° 78-17 du 6 janvier 1978. A ce titre,
+        l'internaute dispose d'un droit d'accès, de modification et de
+        suppression des informations le concernant. Il peut en faire la demande
+        à tout moment par mail via le formulaire de contact du site internet{" "}
+        <Link href={"/"}>www.alatempera.com</Link>
+      </p>
+      <h3>Article 1 4  : Droit applicable</h3>
+      <p>
+        Le présent contrat est régi par la loi française. A défaut d’accord
+        amiable concernant son interprétation ou son exécution, le Tribunal
+        compétent sera celui de Bayonne pour tous litiges ou contentieux.{" "}
+        <span id="anchor"></span>
+      </p>
+      <h3>Article 15 : Relation client </h3>
+      <p>
+        L’utilisateur peut joindre “A la tempera” par le biais du formulaire de
+        contact disponible sur le site{" "}
+        <Link href={"/"}>www.alatempera.com</Link> en suivant ce lien :
+        <Link href="/contact">www.alatempera.com/contact</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Terms;
