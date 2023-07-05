87c9cd5318d7dfc5b1749e6d63ef4d835168b6d3
Antony Merle <antony.merle@gmail.com>
2023-07-05 22:40:13 +0200

    DRY code

--- pages/print/[title].tsx
+++ pages/print/[title].tsx
@@ -9,6 +9,7 @@ import Quantity from "@/components/Quantity";
 import ArrowBack from "@/components/ArrowBack";
 import { getQtyOfSameItemInCart } from "../../components/Quantity";
 import useTranslation from "next-translate/useTranslation";
+import Product from "@/components/Product";
 
 const {
   container,
@@ -23,69 +24,70 @@ const {
 } = style;
 
 const PrintDetails: React.FC<{ print: ICartItem }> = ({ print }) => {
-  console.log({ print });
-  const { onAdd, qty, setQty, setShowCart, cartItems } = useStateContext();
-  const { t, lang } = useTranslation("work");
+  return <Product product={print} />;
+  // console.log({ print });
+  // const { onAdd, qty, setQty, setShowCart, cartItems } = useStateContext();
+  // const { t, lang } = useTranslation("work");
 
-  const handleBuyNow = () => {
-    console.log({ qty });
-    console.log({ print });
+  // const handleBuyNow = () => {
+  //   console.log({ qty });
+  //   console.log({ print });
 
-    onAdd(print, qty);
-    setQty(1);
-    // setShowCart(true);
-  };
+  //   onAdd(print, qty);
+  //   setQty(1);
+  //   // setShowCart(true);
+  // };
 
-  return (
-    <div className={navContainer}>
-      <div className={arrowBackContainer}>
-        <ArrowBack />
-      </div>
-      <div className={container}>
-        <div
-          className={`${imgContainer} ${
-            print.format === "landscape"
-              ? landscapeImgContainer
-              : classicContainer
-          }`}
-        >
-          <Image
-            alt="test"
-            fill={true}
-            objectFit="scale-down"
-            src={print.imgURL[0]}
-          />
-        </div>
-        <div
-          className={`${detailsContainer} ${
-            print.format === "landscape"
-              ? landscapeDetailsContainer
-              : classicContainer
-          }`}
-        >
-          <h2>{print.title}</h2>
-          <h3>{print.priceTTC.toFixed(2)}€</h3>
-          <p>{lang === "en" ? print.description_en : print.description_fr}</p>
-          {getQtyOfSameItemInCart(cartItems, print) >= print.inventory ? (
-            <p>{t("soldOut")}</p>
-          ) : (
-            <div className={btns}>
-              <Quantity context="slug" work={print} />
-              <button
-                onClick={handleBuyNow}
-                disabled={
-                  getQtyOfSameItemInCart(cartItems, print) + qty >
-                  print.inventory
-                }
-              >
-                {t("addToCart")}
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className={navContainer}>
+  //     <div className={arrowBackContainer}>
+  //       <ArrowBack />
+  //     </div>
+  //     <div className={container}>
+  //       <div
+  //         className={`${imgContainer} ${
+  //           print.format === "landscape"
+  //             ? landscapeImgContainer
+  //             : classicContainer
+  //         }`}
+  //       >
+  //         <Image
+  //           alt="test"
+  //           fill={true}
+  //           objectFit="scale-down"
+  //           src={print.imgURL[0]}
+  //         />
+  //       </div>
+  //       <div
+  //         className={`${detailsContainer} ${
+  //           print.format === "landscape"
+  //             ? landscapeDetailsContainer
+  //             : classicContainer
+  //         }`}
+  //       >
+  //         <h2>{print.title}</h2>
+  //         <h3>{print.priceTTC.toFixed(2)}€</h3>
+  //         <p>{lang === "en" ? print.description_en : print.description_fr}</p>
+  //         {getQtyOfSameItemInCart(cartItems, print) >= print.inventory ? (
+  //           <p>{t("soldOut")}</p>
+  //         ) : (
+  //           <div className={btns}>
+  //             <Quantity context="slug" work={print} />
+  //             <button
+  //               onClick={handleBuyNow}
+  //               disabled={
+  //                 getQtyOfSameItemInCart(cartItems, print) + qty >
+  //                 print.inventory
+  //               }
+  //             >
+  //               {t("addToCart")}
+  //             </button>
+  //           </div>
+  //         )}
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default PrintDetails;
--- pages/work/[title].tsx
+++ pages/work/[title].tsx
@@ -1,15 +1,16 @@
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import dbConnect from "@/models/connection";
 import { useState, useCallback } from "react";
 import ImageViewer from "react-simple-image-viewer";
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 import Work from "@/models/works";
 import { ICartItem, useStateContext } from "@/context/StateContext";
 import Image from "next/image";
-import dbConnect from "@/models/connection";
 import Quantity from "@/components/Quantity";
 import { getQtyOfSameItemInCart } from "../../components/Quantity";
 import ArrowBack from "@/components/ArrowBack";
 import useTranslation from "next-translate/useTranslation";
 import style from "../../styles/WorkSlug.module.css";
+import Product from "@/components/Product";
 
 const {
   container,
@@ -24,91 +25,7 @@ const {
 } = style;
 
 const WorkDetails: React.FC<{ work: ICartItem }> = ({ work }) => {
-  console.log({ work });
-  const [isViewerOpen, setIsViewerOpen] = useState(false);
-
-  const { onAdd, qty, setQty, setShowCart, cartItems } = useStateContext();
-  const { t, lang } = useTranslation("work");
-  console.log({ lang });
-
-  const handleBuyNow = () => {
-    console.log({ qty });
-    console.log({ work });
-
-    onAdd(work, qty);
-    setQty(1);
-    // setShowCart(true);
-  };
-
-  const openImageViewer = useCallback(() => {
-    // setCurrentImage(index);
-    setIsViewerOpen(true);
-  }, []);
-
-  return (
-    <div className={navContainer}>
-      <div className={arrowBackContainer}>
-        <ArrowBack />
-      </div>
-
-      <div className={container}>
-        {/* adapt the format of container for better use of screen real estate */}
-        <div
-          className={`${imgContainer} ${
-            work.format === "landscape"
-              ? landscapeImgContainer
-              : classicContainer
-          }`}
-        >
-          <Image
-            alt={work.title}
-            // width={965}
-            // height={950}
-            fill={true}
-            objectFit="scale-down"
-            src={work.imgURL[0]}
-            onClick={() => openImageViewer()}
-          />
-          {isViewerOpen && (
-            <ImageViewer
-              src={[work.imgURL[1] ?? work.imgURL[0]]}
-              // currentIndex={ currentImage }
-              disableScroll={false}
-              closeOnClickOutside={true}
-              onClose={() => setIsViewerOpen(false)}
-            />
-          )}
-          {/* <img src={work.imgURL[0]}></img> */}
-        </div>
-        <div
-          className={`${detailsContainer} ${
-            work.format === "landscape"
-              ? landscapeDetailsContainer
-              : classicContainer
-          }`}
-        >
-          <h2>{work.title}</h2>
-          <h3>{work.priceTTC.toFixed(2)}€</h3>
-          <p>{lang === "en" ? work.description_en : work.description_fr}</p>
-          {getQtyOfSameItemInCart(cartItems, work) >= work.inventory ? (
-            <p>{t("soldOut")}</p>
-          ) : (
-            <div className={btns}>
-              <Quantity context="slug" work={work} />
-              <button
-                onClick={handleBuyNow}
-                disabled={
-                  getQtyOfSameItemInCart(cartItems, work) + qty > work.inventory
-                }
-              >
-                {t("addToCart")}
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+  return <Product product={work} />;
 };
 
 export default WorkDetails;
