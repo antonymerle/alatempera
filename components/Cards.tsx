fc9e1cb56c4da2a690ff6c43908fd699d35a8c50
Antony Merle <antony.merle@gmail.com>
2023-07-09 22:01:23 +0200

    todo

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -2,17 +2,20 @@ import { ICartItem } from "@/context/StateContext";
 import React from "react";
 import Link from "next/link";
 import Card from "./Card";
+import useTranslation from "next-translate/useTranslation";
 import styles from "@/styles/Home.module.css";
 const { main, gallery } = styles;
 
 const Cards: React.FC<{ products: ICartItem[] }> = ({ products }) => {
   console.log({ products });
+  const { lang } = useTranslation();
 
   const tiles: Array<JSX.Element> = products.map((product, i) => {
     return (
       <Link
         href={
-          (product.type === "original" ? "/work/" : "/print/") + product.title
+          (product.type === "original" ? "/work/" : "/print/") +
+          (lang === "en" ? product.title_fr : product.title_fr) // TODO adapt routing
         }
         key={i}
       >
