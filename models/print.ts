f353b50933084433d9d051660ccfec508befe140
Antony Merle <antony.merle@gmail.com>
2023-07-09 22:24:27 +0200

    prinSchema implements new IWork

--- models/print.ts
+++ models/print.ts
@@ -1,18 +1,20 @@
 import mongoose, { Schema, model } from "mongoose";
 import { IWork } from "./works";
+import { PictureSchema } from "./works";
 
-const printSchema = new Schema<IWork>({
-  title: { type: String, required: true },
+const PrintSchema = new Schema<IWork>({
+  title_fr: { type: String, required: true },
+  title_en: { type: String, required: true },
   description_fr: { type: String, required: false },
   description_en: { type: String, required: false },
   priceHT: { type: Number, required: true },
   priceTTC: { type: Number, required: true },
   inventory: { type: Number, required: true },
-  imgURL: { type: [String], required: true },
+  pictures: { type: [PictureSchema], required: true },
   isSoldOut: { type: Boolean, required: true },
   type: { type: String, required: true },
 });
 
-const Print = mongoose.models.Print || model<IWork>("Print", printSchema);
+const Print = mongoose.models.Print || model<IWork>("Print", PrintSchema);
 
 export default Print;
