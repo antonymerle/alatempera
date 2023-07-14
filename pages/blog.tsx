24cd3f4d9592118c677fd141aec81dd8232677d4
Antony Merle <antony.merle@gmail.com>
2023-07-14 16:05:43 +0200

    search feature in blog page

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { InferGetStaticPropsType } from "next";
 import { GetStaticProps } from "next";
 
@@ -9,14 +9,42 @@ import { PostPreview } from "@/types/types";
 import style from "../styles/blog.module.css";
 import BlogPostPreview from "@/components/blog/mdx/BlogPostPreview";
 
-const { title, postPreviewsContainer } = style;
+const {
+  blogContainer,
+  title,
+  postPreviewsContainer,
+  form,
+  searchBox,
+  titleContainer,
+} = style;
 
 const blog: React.FC<{ postPreviews: PostPreview[] }> = ({ postPreviews }) => {
+  const [search, setSearch] = useState("");
+
+  const results: PostPreview[] =
+    search.length > 1
+      ? postPreviews?.filter(
+          (postPreview) =>
+            postPreview.title.toLowerCase().includes(search.toLowerCase()) ||
+            postPreview.description.toLowerCase().includes(search.toLowerCase())
+        )
+      : postPreviews;
+
   return (
-    <div>
-      <h2 className={title}>Blog</h2>
+    <div className={blogContainer}>
+      <div className={titleContainer}>
+        <h2 className={title}>Blog</h2>
+        <form className={form}>
+          <input
+            className={searchBox}
+            placeholder="Rechercher dans les posts"
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+        </form>
+      </div>
+
       <div className={postPreviewsContainer}>
-        {postPreviews
+        {results
           .sort((a, b) => b.timestamp - a.timestamp)
           .map((postPreview, i) => {
             return (
