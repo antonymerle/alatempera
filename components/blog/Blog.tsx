1fdc0f1f9806adfaf339eaee4a65b744d27d8c5c
Antony Merle <antony.merle@gmail.com>
2023-07-14 23:35:25 +0200

    refactoring by creating a dedicated Blog component

---
+++ components/blog/Blog.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { PostPreview } from "@/types/types";
+import style from "../../styles/Blog.module.css";
+import BlogPostPreview from "@/components/blog/mdx/BlogPostPreview";
+
+const {
+  blogContainer,
+  title,
+  postPreviewsContainer,
+  form,
+  searchBox,
+  titleContainer,
+} = style;
+
+const Blog: React.FC<{ postPreviews: PostPreview[] }> = ({ postPreviews }) => {
+  const [search, setSearch] = useState("");
+
+  const results: PostPreview[] =
+    search.length > 1
+      ? postPreviews?.filter(
+          (postPreview) =>
+            postPreview.title.toLowerCase().includes(search.toLowerCase()) ||
+            postPreview.description.toLowerCase().includes(search.toLowerCase())
+        )
+      : postPreviews;
+
+  return (
+    <div className={blogContainer}>
+      <div className={titleContainer}>
+        <h2 className={title}>Blog</h2>
+        <form className={form}>
+          <input
+            className={searchBox}
+            placeholder="Rechercher dans les posts"
+            onChange={(e) => setSearch(e.target.value)}
+          ></input>
+        </form>
+      </div>
+
+      <div className={postPreviewsContainer}>
+        {results
+          .sort((a, b) => b.timestamp - a.timestamp)
+          .map((postPreview, i) => {
+            return (
+              <div key={i}>
+                <BlogPostPreview postPreview={postPreview} />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,61 +1,14 @@
 import { useState } from "react";
 import { InferGetStaticPropsType } from "next";
 import { GetStaticProps } from "next";
-
 import fs from "fs";
 import { serialize } from "next-mdx-remote/serialize";
 import path from "path";
 import { PostPreview } from "@/types/types";
-import style from "../styles/blog.module.css";
-import BlogPostPreview from "@/components/blog/mdx/BlogPostPreview";
-
-const {
-  blogContainer,
-  title,
-  postPreviewsContainer,
-  form,
-  searchBox,
-  titleContainer,
-} = style;
+import Blog from "@/components/blog/Blog";
 
 const blog: React.FC<{ postPreviews: PostPreview[] }> = ({ postPreviews }) => {
-  const [search, setSearch] = useState("");
-
-  const results: PostPreview[] =
-    search.length > 1
-      ? postPreviews?.filter(
-          (postPreview) =>
-            postPreview.title.toLowerCase().includes(search.toLowerCase()) ||
-            postPreview.description.toLowerCase().includes(search.toLowerCase())
-        )
-      : postPreviews;
-
-  return (
-    <div className={blogContainer}>
-      <div className={titleContainer}>
-        <h2 className={title}>Blog</h2>
-        <form className={form}>
-          <input
-            className={searchBox}
-            placeholder="Rechercher dans les posts"
-            onChange={(e) => setSearch(e.target.value)}
-          ></input>
-        </form>
-      </div>
-
-      <div className={postPreviewsContainer}>
-        {results
-          .sort((a, b) => b.timestamp - a.timestamp)
-          .map((postPreview, i) => {
-            return (
-              <div key={i}>
-                <BlogPostPreview postPreview={postPreview} />
-              </div>
-            );
-          })}
-      </div>
-    </div>
-  );
+  return <Blog postPreviews={postPreviews} />;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
