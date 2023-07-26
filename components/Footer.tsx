2fc995fdc568ad6f91fcd51ebc8931b5c0bd5138
Antony Merle <antony.merle@gmail.com>
2023-07-26 18:02:39 +0200

    cleanup comment

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ const { footerContainer, creditContainer, footerList, active } = style;
 
 // detect current path to underline the right link
 const isLinkActive = (currentPath: string, navbarPath: string) => {
-  // 1. tokenize path ["", "prints", "blog"]
+  // tokenize path ["", "prints", "blog"]
   const currentPathTokens: Array<string> = currentPath
     .split("/")
     .filter((token) => token !== "");
