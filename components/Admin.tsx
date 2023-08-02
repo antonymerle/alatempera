17563112832c6d8e5a2a19e9e42ff862e1a9ce9c
Antony Merle <antony.merle@gmail.com>
2023-08-02 23:19:09 +0200

    Fetching data on the client side

--- components/Admin.tsx
+++ components/Admin.tsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { PostPreview } from "@/types/types";
+import Link from "next/link";
+import AdminPostsTable from "./AdminPostsTable";
 
 const Admin = () => {
   const { data: session } = useSession();
+  const [blogPosts, setBlogPosts] = useState(Array<PostPreview>);
 
-  return <div>Admin</div>;
+  useEffect(() => {
+    fetch("/api/admin")
+      .then((res) => res.json())
+      .then((data) => setBlogPosts(data));
+  }, []);
+
+  return (
+    <div>
+      {/* <ul>{posts}</ul> */}
+      <AdminPostsTable posts={blogPosts} />
+    </div>
+  );
 };
 
 export default Admin;
