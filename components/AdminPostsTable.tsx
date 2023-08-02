81d63ce0ba119dc0c82aeb322f59ef09e5f64383
Antony Merle <antony.merle@gmail.com>
2023-08-02 23:19:34 +0200

    display data in MUI table

---
+++ components/AdminPostsTable.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { PostPreview } from "@/types/types";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "title", headerName: "Titre", width: 300 },
+  { field: "timestamp", headerName: "Date de création", width: 130 },
+  { field: "actions", headerName: "Actions", width: 130 },
+];
+
+const AdminPostsTable: React.FC<{ posts: PostPreview[] }> = ({ posts }) => {
+  const rows = posts.map((p, i) => {
+    return { ...p, id: i };
+  });
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default AdminPostsTable;
