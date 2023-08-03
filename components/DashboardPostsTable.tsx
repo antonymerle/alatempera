794bcdd2dfaca748ad153049c884c309e0c1e4e5
Antony Merle <antony.merle@gmail.com>
2023-08-03 23:53:57 +0200

    added dynamic link in rows

--- components/DashboardPostsTable.tsx
+++ components/DashboardPostsTable.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { PostPreview } from "@/types/types";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Link from "next/link";
 
 const columns: GridColDef[] = [
-  { field: "title", headerName: "Titre", width: 300 },
+  {
+    field: "title",
+    headerName: "Titre",
+    width: 300,
+    renderCell(params) {
+      return <Link href={`/admin/edit/${params.value}`}>{params.value}</Link>;
+    },
+  },
   { field: "timestamp", headerName: "Date de création", width: 130 },
   { field: "actions", headerName: "Actions", width: 130 },
 ];
 
 const DashboardPostsTable: React.FC<{ posts: PostPreview[] }> = ({ posts }) => {
   const rows = posts.map((p, i) => {
-    return { ...p, timestamp: new Date().toLocaleDateString(), id: i };
+    return {
+      ...p,
+      timestamp: new Date().toLocaleDateString(),
+      id: i,
+    };
   });
   return (
     <div style={{ height: 400, width: "100%" }}>
