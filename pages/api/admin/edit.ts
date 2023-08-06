ec0877fc98e3f3f0dde84579b7173345b7711490
Antony Merle <antony.merle@gmail.com>
2023-08-06 10:55:44 +0200

    edit route implementation

---
+++ pages/api/admin/edit.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// TODO : protect ROUTE
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { postPath, values } = req.body;
+    const newPostContent = matter.stringify(values.content, {
+      title: values.title,
+      description: values.description,
+      previewImage: values.previewImage,
+      timestamp: values.timestamp,
+    });
+
+    try {
+      fs.writeFileSync(postPath, newPostContent, {
+        encoding: "utf-8",
+        flag: "w",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
