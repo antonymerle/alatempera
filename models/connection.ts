9c54585028de20ffca5d13543ed9fde48cf40f48
Antony Merle <antony.merle@gmail.com>
2023-08-10 15:45:48 +0200

    connection string naming resolve mongoose error ??

--- models/connection.ts
+++ models/connection.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-  const connectionString = process.env.DB_CONNECTION_STRING!;
+  const connectionString = process.env.DB_CONNECTION_STRING_2!;
 
   mongoose
     .connect(connectionString, { connectTimeoutMS: 3000 })
