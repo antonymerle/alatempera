479b9ba38f09a4ccce90e836b644850c315d5ff5
Antony Merle <antony.merle@gmail.com>
2023-08-13 22:44:51 +0200

    delete unused component

--- components/Editor.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useRouter } from "next/router";
-
-const Editor: React.FC<{ path: string }> = ({ path }) => {
-  const { pathname: currentPathname } = useRouter();
-  return <div>Editor</div>;
-};
-
-export default Editor;
