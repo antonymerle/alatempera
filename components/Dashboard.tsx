204cc6f1ab02149c7ff20e614fd8209dce9e5cb9
Antony Merle <antony.merle@gmail.com>
2023-08-14 16:11:03 +0200

    add create post option in dashboard

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -4,6 +4,9 @@ import { PostPreview } from "@/types/types";
 import Link from "next/link";
 import DashboardPostsTable from "./DashboardPostsTable";
 import Router from "next/router";
+import style from "../styles/Dashboard.module.css";
+
+const { container } = style;
 
 const Dashboard = () => {
   const { data: session } = useSession();
@@ -24,7 +27,15 @@ const Dashboard = () => {
   return (
     <div>
       {session ? (
-        <DashboardPostsTable posts={blogPosts} />
+        <div className={container}>
+          <h2>Administration du site</h2>
+          <h3>Créer un nouveau post</h3>
+          <Link href={"/admin/create/newPost"}>
+            <button>Créer un nouveau post</button>
+          </Link>
+          <h3>Editer un post existant</h3>
+          <DashboardPostsTable posts={blogPosts} />
+        </div>
       ) : (
         <div>
           <h2>Unauthorized</h2>
