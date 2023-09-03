832527b51cdade8e362dc42f643b8ce41b495484
Antony Merle <antony.merle@gmail.com>
2023-09-03 15:18:46 +0200

    dynamic switch between about/technique

--- components/About.tsx
+++ components/About.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import useTranslation from "next-translate/useTranslation";
+import { HiPaintBrush } from "react-icons/hi2";
 import style from "../styles/About.module.css";
 
-const { aboutContainer, imgContainer, aboutImage, textContainer } = style;
+const { aboutContainer, imgContainer, aboutImage, textContainer, tab } = style;
+
+enum Content {
+  about = "aboutContent",
+  technique = "techniqueContent",
+}
 
 const About = () => {
+  const [content, setContent] = useState<Content>(Content.about);
   const { t } = useTranslation("about");
 
-  const markup = t("mainContent")
+  const markup = t(content)
     .split("\n")
     .map((p, i) => {
       return (
@@ -26,15 +33,31 @@ const About = () => {
       <div className={imgContainer}>
         <Image
           className={aboutImage}
-          src={"/manon.jpg"}
+          src={content === Content.about ? "/manon.jpg" : "/technique.jpg"}
           width={936}
           height={1407}
           alt="Manon working at her desk"
         />
       </div>
       <div className={textContainer}>
-        <h2>{t("about")}</h2>
+        <h2>
+          {t(content === Content.about ? "aboutTitle" : "techniqueTitle")}
+        </h2>
         <div>{markup}</div>
+        <div className={tab}>
+          <button
+            onClick={() =>
+              setContent(
+                content === Content.about ? Content.technique : Content.about
+              )
+            }
+          >
+            {t(content === Content.about ? "techniqueTitle" : "aboutTitle")}
+            {content === Content.about && (
+              <HiPaintBrush style={{ marginLeft: "1em" }} />
+            )}
+          </button>
+        </div>
       </div>
     </div>
   );
