f18a335e21daab3a604b85564e8c0f05246bbd45
Antony Merle <antony.merle@gmail.com>
2023-09-22 22:50:17 +0200

    no cors

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -45,7 +45,7 @@ const Cart = () => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Origin": "*",
         "Accept-Language": lang,
       },
       body: JSON.stringify({ cartItems }),
