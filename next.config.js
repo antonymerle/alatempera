7ba498d1222de4480ed8591c0c43b50cd86af026
Antony Merle <antony.merle@gmail.com>
2023-09-23 00:24:58 +0200

    fs fallback

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextTranslate = require("next-translate-plugin");
 
 const nextConfig = {
   // webpack5: true,
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false };
-  //   return config;
-  // },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
   reactStrictMode: true,
   // images: {
   //   domains: ["source.unsplash.com", "images.unsplash.com"],
