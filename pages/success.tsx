f89f43415d09146e77cd58fb1d2a0d81742011aa
Antony Merle <antony.merle@gmail.com>
2023-09-23 10:38:06 +0200

    payload length

--- pages/success.tsx
+++ pages/success.tsx
@@ -14,13 +14,17 @@ export const getServerSideProps: GetServerSideProps<{
   // const session = await await stripe.checkout.sessions.retrieve(req.query.session_id);
   const stripeSessionID = context.query.session_id;
 
+  const payloadLength = Buffer.byteLength(
+    JSON.stringify({ session_id: stripeSessionID })
+  ).toString();
+
   try {
     const res = await fetch(`${process.env.BASE_DOMAIN_URL}/api/checkout`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         // "Access-Control-Allow-Origin": "*",
-        // "Content-length": "0",
+        "Content-length": payloadLength,
       },
       body: JSON.stringify({ session_id: stripeSessionID }),
     });
