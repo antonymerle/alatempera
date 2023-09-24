17423bb673dc439aed50fe9a13845e239e941676
Antony Merle <antony.merle@gmail.com>
2023-09-24 16:34:05 +0200

    big timeout and nullish coalesce

--- models/connection.ts
+++ models/connection.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const dbConnect = async () => {
-  const connectionString = process.env.DB_CONNECTION_STRING!;
+  const connectionString = process.env.DB_CONNECTION_STRING ?? "nope";
 
   mongoose
-    .connect(connectionString, { connectTimeoutMS: 3000 })
+    .connect(connectionString, { connectTimeoutMS: 15000 })
     .then(() => console.log("Connecté à la base de données"))
     .catch((error) => console.log(error));
 };
