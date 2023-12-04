9538211828ce49f059fe47f5e7484829e8cf8f0b
Antony Merle <antony.merle@gmail.com>
2023-12-04 21:37:57 +0100

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from "@/components/Layout";
 import { StateContext } from "@/context/StateContext";
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Layout>
           <Toaster />
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </StateContext>
     </SessionProvider>
